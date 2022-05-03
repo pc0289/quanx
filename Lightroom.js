de493271aa702e87cc49498a841dd08f029da3fd
pc0289 <41770790+pc0289@users.noreply.github.com>
2022-05-03 18:25:34 +0800

    Update Lightroom.js

--- Lightroom.js
+++ Lightroom.js
@@ -1,6 +1,6 @@
-let body = JSON.parse($response.body);
-body.entitlement.status = "subscriber";
-body.current_subs = {
+let obj = JSON.parse($response.body);
+obj.entitlement.status = "subscriber";
+obj.current_subs = {
   "product_id": "lightroom",
   "store": "adobe",
   "purchase_date": "2019-10-10T16:32:10.254954Z",
@@ -22,14 +22,14 @@ body.current_subs = {
     "storage_quota": "100"
   }
 };
-body.entitlement.storage = {
+obj.entitlement.storage = {
   "used": 0,
   "limit": 1154487209165,
   "display_limit": 1099511627776,
   "warn": 992137445376
 };
-body.avatar.placeholder = true;
+obj.avatar.placeholder = true;
 
 $done({
-  body: JSON.stringify(body)
+  body: JSON.stringify(obj)
 });
